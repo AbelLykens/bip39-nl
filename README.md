f110487217e89f5fcc28c4174abbb6bf51e3e3d3
Abel <abellykens@protonmail.com>
2023-08-11 12:31:31 +0000

    Generator en sources

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # bip39-nl
+
+Download https://opus.nlpl.eu/download.php?f=OpenSubtitles/v2016/xml/nl.zip and extract
+
+mkdir source
+wget -O source/nl.zip
+cd source
+`unzip nl.zip OpenSubtitles/xml/nl/2014/*`
+mkdir xml
+`find OpenSubtitles/ -type f -name "*xml" -exec mv {} xml/ ';'`
+cd ..
---
+++ extractwords.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+import sqlite3
+import os
+import re
+
+
+con = sqlite3.connect("words-nl.db")
+cur = con.cursor()
+
+# Table
+cur.execute("DROP table words")
+cur.execute("CREATE TABLE IF NOT EXISTS words (source, word);")
+
+# Source files
+dir_list = os.listdir('source/xml')
+num_files = len(dir_list)
+i=0
+
+for xmlfile in dir_list:
+    i+=1
+    print(f"{i}/{num_files}")
+
+    path = f"source/xml/{xmlfile}"
+    with open(f"source/xml/{xmlfile}",'r') as f:
+        content = f.read()
+        #print(content)
+        words = re.findall("<w[^>]*>([a-zA-Z]{4,8})</w>",content)
+        data = []
+        for word in words:
+            data.append( (path,word.lower()) )
+
+        cur.executemany("INSERT INTO words VALUES(?, ?)", data)
+        con.commit()
+
+cur.execute("select count(word) num,word from words group by word order by num DESC limit 4096;")
+with open("result.csv","w") as output:
+    for res in cur.fetchall():
+        line = f"{res[1]}"
+        print(line)
+        output.write(line + "\n")
+        
