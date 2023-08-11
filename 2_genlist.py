4191a514d424b957dfbb9bcf0c21d25913d345b9
Abel <abellykens@protonmail.com>
2023-08-11 15:33:33 +0000

    Also check for first 3 chars

--- 2_genlist.py
+++ 2_genlist.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+seenfirst3 = []
 seenfirst4 = []
 goodwords = []
 
@@ -11,13 +12,17 @@ with open('result_uniquefirst4.csv','w') as resfile:
     with open('result.csv') as openfile:
         for line in openfile:
             word = line.rstrip()
+            first3 = word[0:3]
             first4 = word[0:4]
     
             if first4 in seenfirst4:
                 continue
             if word not in goodwords:
                 continue
+            if seenfirst3.count(first3) > 2:
+                continue
         
+            seenfirst3.append(first3)
             seenfirst4.append(first4)
             print(word)
             resfile.write(word+"\n")
